925247c31d606b2c8906b43c02203a545bbc32ec
Hélio Márcio Filho <heliommsfilho@tutanota.com>
2019-05-15 21:59:09 +0100

    3.4. Consultando primeiro recurso com GET

---
+++ src/main/java/com/heliommsfilho/sistemafinanceiro/api/SistemaFinanceiroApiApplication.java
@@ -0,0 +1,13 @@
+package com.heliommsfilho.sistemafinanceiro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaFinanceiroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaFinanceiroApiApplication.class, args);
+	}
+
+}
