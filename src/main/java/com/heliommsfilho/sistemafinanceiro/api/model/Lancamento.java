2934530afdc9d03883c7d2d98e142b279f1476d3
Hélio Márcio Filho <heliommsfilho@tutanota.com>
2019-06-10 18:56:46 +0100

    5.5 Validando lançamento com Bean Validation

--- src/main/java/com/heliommsfilho/sistemafinanceiro/api/model/Lancamento.java
+++ src/main/java/com/heliommsfilho/sistemafinanceiro/api/model/Lancamento.java
@@ -14,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import com.heliommsfilho.sistemafinanceiro.api.model.enums.TipoLancamento;
 
@@ -32,25 +33,32 @@ public class Lancamento implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
+	
+	@NotNull
 	private String descricao;
 	
+	@NotNull
 	@Column(name = "data_vencimento")
 	private LocalDate dataVencimento;
 	
 	@Column(name = "data_pagamento")
 	private LocalDate dataPagamento;
 	
+	@NotNull
 	private BigDecimal valor;
 	
 	private String observacao;
 	
+	@NotNull
 	@Enumerated(EnumType.STRING)
 	private TipoLancamento tipo;
 	
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "codigo_categoria")
 	private Categoria categoria;
 	
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "codigo_pessoa")
 	private Pessoa pessoa;
