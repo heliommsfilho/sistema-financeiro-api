77f73c438a1b01ce1bd7f518daa5dc8d4b6a49b6
Hélio Márcio Filho <heliommsfilho@tutanota.com>
2019-07-06 17:23:15 +0100

    6.14. Implementando o logout

---
+++ src/main/java/com/heliommsfilho/sistemafinanceiro/api/resource/TokenResource.java
@@ -0,0 +1,27 @@
+package com.heliommsfilho.sistemafinanceiro.api.resource;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tokens")
+public class TokenResource {
+
+	@DeleteMapping("/revoke")
+	public void revoke(HttpServletRequest req, HttpServletResponse resp) {
+		Cookie refreshTokenCookie = new Cookie("refreshToken", null);
+		refreshTokenCookie.setHttpOnly(true);
+		refreshTokenCookie.setSecure(false);
+		refreshTokenCookie.setPath(req.getContextPath() + "/ouaht/token");
+		refreshTokenCookie.setMaxAge(0);
+		
+		resp.addCookie(refreshTokenCookie);
+		resp.setStatus(HttpStatus.NO_CONTENT.value());
+	}
+}
